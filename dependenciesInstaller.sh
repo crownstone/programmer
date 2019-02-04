ebfa7815680b80ff6d82193b6367a6df287d245e
alexdm0 <alexdemulder@gmail.com>
2019-02-04 16:59:36 +0100

    basic structure

--- dependenciesInstaller.sh
+++ dependenciesInstaller.sh
@@ -1,14 +1,9 @@
+#!/bin/bash
 
-# install python 3.7
+# install python 3.7 dependencies (should already be there due to the python37Installer)
 sudo apt-get update -y
 sudo apt-get install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev -y
 
-wget https://www.python.org/ftp/python/3.7.0/Python-3.7.0.tar.xz
-tar xf Python-3.7.0.tar.xz
-cd Python-3.7.0
-./configure
-make -j 4
-sudo make altinstall
-
 # install python lib dependencies
 sudo apt-get install build-essential libbluetooth-dev libglib2.0-dev python3-setuptools
+
