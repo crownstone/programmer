54ab913191e90e659dbd7eb45045c3898f298cf0
alexdm0 <alexdemulder@gmail.com>
2020-06-03 16:26:21 +0200

    update lib

--- lib/crownstone_uart/core/uart/UartManager.py
+++ lib/crownstone_uart/core/uart/UartManager.py
@@ -28,8 +28,7 @@ class UartManager:
         if self._uartBridge is not None:
             self._uartBridge.stop_sync()
 
-
-    async def initialize(self, port = None, baudrate = 230400):
+    async def initialize(self, port=None, baudrate=230400):
         self.baudRate = baudrate
         self.ready = False
 
@@ -44,33 +43,32 @@ class UartManager:
                 index += 1
             if not found_port:
                 return await self.setupConnection(port)
-
+            else:
+                await self._attemptConnection(self._attemptingIndex, False)
 
         if self._trackingLoop is None:
             self._trackingLoop = asyncio.get_running_loop()
 
         if self.port is None:
-            if self._attemptingIndex >= len(self._availablePorts): # this also catches len(self._availablePorts) == 0
+            if self._attemptingIndex >= len(self._availablePorts):  # this also catches len(self._availablePorts) == 0
                 print("No Crownstone USB connected? Retrying...")
                 await asyncio.sleep(1)
                 await self.reset()
             else:
                 await self._attemptConnection(self._attemptingIndex)
 
-
-    async def _attemptConnection(self, index):
+    async def _attemptConnection(self, index, handshake=True):
         attemptingPort = self._availablePorts[index]
-        await self.setupConnection(attemptingPort.device)
+        await self.setupConnection(attemptingPort.device, handshake)
 
-
-    async def setupConnection(self, port):
+    async def setupConnection(self, port, handshake=True):
         self._uartBridge = UartBridge(port, self.baudRate)
         self._uartBridge.start()
         await self._uartBridge.starting()
-
-        # success = await self._uartBridge.handshake()
         success = True
 
+        if handshake:
+            success = await self._uartBridge.handshake()
         if not success:
             print("Crownstone handshake failed. Moving on to next device...")
             self._attemptingIndex += 1
